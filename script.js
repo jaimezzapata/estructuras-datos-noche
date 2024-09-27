1fa14c17576109fa39ee30e698d4e96902c80185
jaimezzapata <zapataval2304@gmail.com>
2024-09-26 19:12:37 -0500

    Introducción a los arreglos: posiciones y tamaños

---
+++ script.js
@@ -0,0 +1,9 @@
+let numero = 23
+let variasCosas = [23, true, 'Jaime', function(){}, [], numero]
+let otroArreglo = []
+
+console.log(variasCosas[0] * variasCosas[5])
+console.log(variasCosas[1])
+console.log(variasCosas[5])
+console.log(variasCosas[3])
+console.log(otroArreglo)
